21a9d8ffd23c5a5562f59dbc9d368c031013ee78
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2017-11-29 14:52:01 +0300

    Turn off most of devServer stats for faster compilation

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,23 @@ module.exports = {
     },
     devServer: {
         hot: true,
-        contentBase: "./dist"
+        contentBase: "./dist",
+        stats: {
+            colors: true,
+            hash: false,
+            version: false,
+            timings: false,
+            assets: false,
+            chunks: false,
+            modules: false,
+            reasons: false,
+            children: false,
+            source: false,
+            errors: false,
+            errorDetails: false,
+            warnings: false,
+            publicPath: false
+        }
     },
 
     module: {
