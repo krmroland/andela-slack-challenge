2f004c9df1bfcea86549758f66cf8a9bda1f2ca9
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2017-11-29 07:00:23 +0300

    add sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/js/app.js",
+    entry: ["./src/js/app.js", "./src/sass/app.scss"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "app.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["css-loader", "sass-loader"]
+            }
+        ]
     }
 };
