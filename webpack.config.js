d62a415ccff381d78c7eb5912e20efc431e2d697
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2017-11-29 11:21:02 +0300

    Extract css from our build

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,36 @@
 const path = require("path");
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const webpack = require("webpack");
+
 module.exports = {
     entry: ["./src/js/app.js", "./src/sass/app.scss"],
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "app.js"
+        filename: "[name].js"
     },
+
     module: {
         rules: [
             {
+                test: /\.css$/,
+
+                use: ["style-loader", "css-loader"]
+            },
+            {
                 test: /\.scss$/,
-                use: ["css-loader", "sass-loader"]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "app.css",
+            allChunks: true
+        })
+    ]
 };
